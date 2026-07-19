27dd9c68bfdb64462f2c286db41775ed02243db4
Kuan-Yu Yu <kxy500@student.bham.ac.uk>
2026-07-19 18:23:00 +0100

    fix(views): use ai_response variable instead of undefined ai_answering

--- basic/views.py
+++ basic/views.py
@@ -1,57 +1,41 @@
 from django.shortcuts import render
-from .forms import QueryForm
+from .forms import *
 from .models import *
 from rag_agent.linear_rag import linear_rag_respones
+from rag_agent.capped_agentic_rag import graph as capped_rag
+from rag_agent.uncapped_agentic_rag import graph as uncapped_rag
+import random
 
 # Create your views here.
 
 
-def linear(request):
+def index(request):
+    sentence = random.choice(['How may I assist you today?', 'What can I do for you today?',
+                              'Please let me know how I can be of assistance.', 'How can I help you today?', 'How can I help you out today?', 'How can I make your day easier today?'])
+    ai_response = ""
     user_query = ""
-    llm_answering = ""
-    title = "linear"
+    histories = UserQuery.objects.all()
+    mode = request.GET.get("mode")
 
     if request.method == "POST":
         form = QueryForm(request.POST)
         if form.is_valid():
-            user_query = form.cleaned_data["query"]
-            llm_answering = linear_rag_respones(user_query)
+            user_query = form.cleaned_data['user_query']
 
+            if mode == "linear":
+                ai_response = linear_rag_respones(user_query=user_query)
+            elif mode == "uncapped":
+                ai_response = uncapped_rag.invoke({"messages": user_query})
+            elif mode == "capped":
+                ai_response = capped_rag.invoke(
+                    {"messages": user_query, "rewrite_count": 0})
+
+        UserQuery.objects.create(
+            user_query=user_query, ai_mode=mode, ai_response=ai_answering)
     else:
         form = QueryForm()
-
-    return render(request, "test.html", {"form": form, "user_query": user_query, "llm_answering": llm_answering, "title": title})
-
-
-def uncapped(request):
-    user_query = ""
-    llm_answering = ""
-    title = "uncapped"
-
-    if request.method == "POST":
-        form = QueryForm(request.POST)
-        if form.is_valid():
-            user_query = form.cleaned_data["query"]
-            llm_answering = linear_rag_respones(user_query)
-
-    else:
-        form = QueryForm()
-
-    return render(request, "uncapped.html", {"form": form, "user_query": user_query, "llm_answering": llm_answering, "title": title})
+    return render(request, 'index.html', {'form': form, 'histories': histories, 'sentence': sentence, "mode": mode})
 
 
-def capped(request):
-    user_query = ""
-    llm_answering = ""
-    title = "capped"
-
-    if request.method == "POST":
-        form = QueryForm(request.POST)
-        if form.is_valid():
-            user_query = form.cleaned_data["query"]
-            llm_answering = linear_rag_respones(user_query)
-
-    else:
-        form = QueryForm()
-
-    return render(request, "capped.html", {"form": form, "user_query": user_query, "llm_answering": llm_answering, "title": title})
+def dashboard(request):
+    pass
