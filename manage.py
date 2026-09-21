7a09d0a28b47e31c08140805723ec39e96e2ca4b
Kuan-Yu Yu <kxy500@student.bham.ac.uk>
2026-09-21 12:40:18 +0200

    docs: delete unwanted code

--- manage.py
+++ manage.py
@@ -2,15 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
-
-load_dotenv()
-
-os.environ["LANGSMITH_TRACING"] = "true"
-os.environ["LANGSMITH_ENDPOINT"] = "https://eu.api.smith.langchain.com"
-os.environ["LANGSMITH_PROJECT"] = "Tracking RAG system"
-os.environ["LANGCHAIN_HIDE_INPUTS"] = "true"
-os.environ["LANGCHAIN_HIDE_OUTPUTS"] = "true"
 
 
 def main():
